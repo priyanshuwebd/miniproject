5b8a7385f360c8e7449d56e9a2129fcfe9c92e08
priyanshupande <priyanshupande23@gmail.com>
2024-12-08 14:31:48 +0530

    miniproject uploade

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1 className="text-center text-danger,text-capitalize my-5">welcome to my channel</h1><div className="container text-center">
+        <div class="row row-cols-2">
+          <div class="col"><div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+
+              <div class="card">
+                <img src="https://picsum.photos/200" className="card-img-top" alt="..."height="200px" />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card">
+                <img src="https://picsum.photos/201" className="card-img-top" alt="..." height="200px" />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card">
+                <img src="https://picsum.photos/203"className="card-img-top" alt="..."height="200px" />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col">
+              <div class="card">
+                <img src="https://picsum.photos/202"className="card-img-top" alt="..." height="200px" />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+          </div></div>
+
+        </div>
+      </div>
+      </React.Fragment>
+
+  );
+};
+
+export default App;
+
+
+
+
+
+
+{/* <h1 className="text-center text-danger text-capitalize my-5">welcome to my channel</h1>
+      <div className="container ">
+        <div className="row">
+          <div className="col-sm"><div class="card">
+
+            <img src="https://picsum.photos/200/300" class="card-img-top" alt="..." height="200px" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+
+          </div> </div>
+          <div className="col-sm"><div class="card" >
+            <img src="https://picsum.photos/200/301" class="card-img-top" alt="..." height="200px" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+
+          </div></div>
+          <div className="col-sm"><div class="card" >
+            <img src="https://picsum.photos/201/300" class="card-img-top" alt="..." height="200px" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div></div> */}
+{/* </div>
+      </div> */}
+
